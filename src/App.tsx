41f005a62ee281c0f9fccbfd5fe88fe17a20ed4f
Anurag Shrestha <anuragshrestha16@gmail.com>
2026-02-13 21:51:17 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+ <div className='app-root'>
+  <header className='app-header'>
+  <h1>Splotify</h1>
+  </header>
+ </div>
+  )
+}
+
+export default App
