2d36f70d8cadfcedd27aaa150e07b2da434a4e89
Anurag Shrestha <anuragshrestha16@gmail.com>
2026-02-15 20:29:47 -0700

    added plumber image

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import tickIcon from '../src/assets/icons/tickIcon.svg'
+import tickIcon from "../src/assets/icons/tickIcon.svg";
+import plumberImage from '../src/assets/plumberImage.png'
 
 function App() {
   return (
@@ -8,8 +9,8 @@ function App() {
         <div className="brand">
           <span className="brand-mark" aria-hidden="true">
             <span className="mark-square mark-square--top" />
-            <span className="mark-square mark-square--middle"/>
-            <span className="mark-square mark-square--bottom"/>
+            <span className="mark-square mark-square--middle" />
+            <span className="mark-square mark-square--bottom" />
           </span>
           <h1 className="app-title">Splotify</h1>
         </div>
@@ -22,29 +23,39 @@ function App() {
           </button>
         </nav>
       </header>
-      <div className="home-content">
-        {/** left side */}
-        <div>
-         <div className="home-verified-text">
-          <img src={tickIcon} className="tick-icon" alt="" aria-hidden="true"/>
-          <p>
-            VERIFIED PROFESSIONALS
-          </p>
-         </div>
-        </div>
-        <h1 className="title">
-          <span className="title-blue"> Expert help for your home,</span>
-          <span className="title-white"> just a click away.</span>
-        </h1>
-        <p className="connecting-homeowners">
-          Connecting homeowners with top-rated professional contractors for plumbing, electrical, HVAC, and more.
-        </p>
+      <section className="home-content">
+        <div className="home-layout">
+          {/** left side */}
+          <div className="home-left">
+            <div className="home-verified-text">
+              <img
+                src={tickIcon}
+                className="tick-icon"
+                alt=""
+                aria-hidden="true"
+              />
+              <p>VERIFIED PROFESSIONALS</p>
+            </div>
 
-        {/** right side */}
-        <div>
-
+            <h1 className="title">
+              <span className="title-blue"> Expert help for your home,</span>
+              <span className="title-white"> just a click away.</span>
+            </h1>
+            <p className="connecting-homeowners">
+              Connecting homeowners with top-rated professional contractors for
+              plumbing, electrical, HVAC, and more.
+            </p>
+          </div>
+          {/** right side */}
+          <div className="home-right">
+            <img
+              className="hero-image"
+              src={plumberImage}
+              alt=""
+            />
+          </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 }
