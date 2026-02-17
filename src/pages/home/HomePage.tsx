1333a5893a2192eeaec0f14f8095da56a96a406d
Anurag Shrestha <anuragshrestha16@gmail.com>
2026-02-16 21:30:53 -0700

    added popular pages

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,12 +1,14 @@
 import AppHeader from "../../components/layout/AppHeader";
 import HeroSection from "./sections/HeroSection";
 import styles from "./HomePage.module.css";
+import PopularServices from "./popularServices/PopularServices";
 
 function HomePage() {
   return (
     <div className={styles.root}>
       <AppHeader />
       <HeroSection />
+      <PopularServices/>
     </div>
   );
 }
