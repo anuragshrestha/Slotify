98fd2f3fa20262258a3af5ab35018e85a823ff87
Anurag Shrestha <anuragshrestha16@gmail.com>
2026-02-18 23:40:28 -0700

    renamed the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
-import SignUp from "./pages/auth/SignUp";
+import SignUp from "./pages/auth/SignupPage";
 
 function App() {
   return (
